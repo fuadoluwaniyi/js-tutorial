2c61bcdf7170511507b012fdf5dc74b74db01563
fuadoluwaniyi <106689970+fuadoluwaniyi@users.noreply.github.com>
2022-11-11 10:04:36 +0100

    first commit

---
+++ sandbox.js
@@ -0,0 +1,123 @@
+// // strings
+// console.log('hello, world');
+
+// let email = 'fuadoluwaniyi@gmail.com';
+// console.log(email);
+
+// // string concatenation
+// let firstName = 'Fuad';
+// let lastName = 'Adeshina';
+
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// // getting characters
+// console.log(fullName[2]);
+
+// // string length
+// console.log(fullName.length);
+
+// // string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result);
+
+// let index = email.indexOf('@');
+// console.log(index);
+
+// // for loops
+
+// // for(let i = 0; i < 5; i++){
+// //     console.log('in loop:', i);
+// // }
+
+// // console.log('loop finished');
+
+// const names  = ['shaun', 'mario', 'luigi'];
+
+// for(let i = 0; i < names.length; i++){
+//     // console.log(names[i]);
+//     let html = `<div>${names[i]}</div>`;
+//     console.log(html);
+// }
+
+// while loop
+// let i = 0;
+
+// while(i < 5){
+//     console.log('in loop:', i);
+//     i++;
+// }
+
+// const names = ['fuad', 'niyi', 'dipo'];
+
+// let i = 0;
+// while(i < names.length){
+//     console.log(names[i]);
+//     i++;
+// }
+
+// do while loops
+
+// let i = 0;
+
+// do{
+//     console.log('val of i is:', i);
+//     i++;
+// } while(i < 5);
+
+// const age = 25;
+
+// if(age > 20){
+//     console.log('you are over 20 years old');
+// }
+
+// const gamers = ['fuad', 'muhaj', 'isolab', 'niyi'];
+
+// if(gamers.length > 3){
+//     console.log("that's alot of gamers");
+// }
+
+// const password = 'pass';
+// if(password.length < 8){
+//     console.log('password is not long enough');
+// }
+// if(password.length >= 8){
+//     console.log('password is long enough');
+// }
+
+
+// const password = 'password1234';
+// if(password.length >= 12){
+//     console.log('password is strong');
+// } else if(password.length >= 8){
+//     console.log('password is long enough');
+// } else{
+//     console.log('password is not long enough');  
+// }
+
+// logical operators - OR || and AND &&
+
+
+// const password = 'pass@1';
+// if(password.length >= 12 && password.includes('@')){
+//     console.log('password is High');
+// } else if(password.length >= 8 || password.includes('@') && password.length > 5){
+//     console.log('password is strong');
+// } else{
+//     console.log('password is not long enough');  
+// }
+
+// logical NOT (!)
+
+// let user = false;
+
+// if(!user){
+//     console.log('you must be logged in to continue');
+// }
+
+// console.log(!true);
+// console.log(!false);
+
+// break and continue 
+
